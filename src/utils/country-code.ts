dfd6679f8c96e6861ce9b49763a633978e5c83bf
xieleihan <xieleihan@zerozero.cn>
2024-12-17 09:26:02 +0800

    feat:补充工具函数等

---
+++ src/utils/country-code.ts
@@ -0,0 +1,195 @@
+// 适用国家列表
+export const countryColumns = [
+    {
+        shop: 'uk', //英国
+        value: 'UK',
+        label: 'United Kingdom',
+        countryCode: 'gb'
+    },
+    {
+        shop: 'us', //美国
+        value: 'US',
+        label: 'United States',
+        countryCode: 'us'
+    },
+    {
+        shop: 'eu', //法国
+        value: 'France',
+        label: 'France',
+        countryCode: 'fr'
+    },
+    {
+        shop: 'eu', //德国
+        value: 'Germany',
+        label: 'Germany',
+        countryCode: 'de'
+    },
+    {
+        shop: 'eu', //荷兰
+        value: 'Netherlands',
+        label: 'Netherlands',
+        countryCode: 'nl'
+    },
+    {
+        shop: 'eu', //西班牙
+        value: 'Spain',
+        label: 'Spain',
+        countryCode: 'es'
+    },
+    {
+        shop: 'eu', //意大利
+        value: 'Italia',
+        label: 'Italia',
+        countryCode: 'it'
+    },
+    {
+        shop: 'eu', //奥地利
+        value: 'Austria',
+        label: 'Austria',
+        countryCode: 'at'
+    },
+    {
+        shop: 'eu', //比利时
+        value: 'Belgium',
+        label: 'Belgium',
+        countryCode: 'be'
+    },
+    {
+        shop: 'eu', //保加利亚
+        value: 'Bulgaria',
+        label: 'Bulgaria',
+        countryCode: 'bg'
+    },
+    {
+        shop: 'eu', //克罗地亚
+        value: 'Croatia',
+        label: 'Croatia',
+        countryCode: 'hr'
+    },
+    {
+        shop: 'eu', //捷克
+        value: 'Czech Republic',
+        label: 'Czech Republic',
+        countryCode: 'cz'
+    },
+    {
+        shop: 'eu', //丹麦
+        value: 'Denmark',
+        label: 'Denmark',
+        countryCode: 'dk'
+    },
+    {
+        shop: 'eu', //爱沙尼亚
+        value: 'Estonia',
+        label: 'Estonia',
+        countryCode: 'ee'
+    },
+    {
+        shop: 'eu', //芬兰
+        value: 'Finland',
+        label: 'Finland',
+        countryCode: 'fi'
+    },
+    {
+        shop: 'eu', //匈牙利
+        value: 'Hungary',
+        label: 'Hungary',
+        countryCode: 'hu'
+    },
+    {
+        shop: 'eu', //爱尔兰
+        value: 'Ireland',
+        label: 'Ireland',
+        countryCode: 'ie'
+    },
+    {
+        shop: 'eu', //拉脱维亚
+        value: 'Latvia',
+        label: 'Latvia',
+        countryCode: 'lv'
+    },
+    {
+        shop: 'eu', //立陶宛
+        value: 'Lithuania',
+        label: 'Lithuania',
+        countryCode: 'lt'
+    },
+    {
+        shop: 'eu', //卢森堡
+        value: 'Luxembourg',
+        label: 'Luxembourg',
+        countryCode: 'lu'
+    },
+    {
+        shop: 'eu', //摩纳哥
+        value: 'Monaco',
+        label: 'Monaco',
+        countryCode: 'mc'
+    },
+    {
+        shop: 'eu', //波兰
+        value: 'Poland',
+        label: 'Poland',
+        countryCode: 'pl'
+    },
+    {
+        shop: 'eu', //葡萄牙
+        value: 'Portugal',
+        label: 'Portugal',
+        countryCode: 'pt'
+    },
+    {
+        shop: 'eu', //罗马尼亚
+        value: 'Romania',
+        label: 'Romania',
+        countryCode: 'ro'
+    },
+    {
+        shop: 'eu', //斯洛伐克
+        value: 'Slovakia',
+        label: 'Slovakia',
+        countryCode: 'sk'
+    },
+    {
+        shop: 'eu', //斯洛文尼亚
+        value: 'Slovenia',
+        label: 'Slovenia',
+        countryCode: 'si'
+    },
+    {
+        shop: 'eu', //瑞典
+        value: 'Sweden',
+        label: 'Sweden',
+        countryCode: 'se'
+    },
+    {
+        shop: 'global', //挪威
+        value: 'Norway',
+        label: 'Norway',
+        countryCode: 'no'
+    },
+    {
+        shop: 'global', //瑞士
+        value: 'Switzerland',
+        label: 'Switzerland',
+        countryCode: 'ch'
+    },
+    {
+        shop: 'global', //加拿大
+        value: 'Canada',
+        label: 'Canada',
+        countryCode: 'cs'
+    },
+    {
+        shop: 'global', //澳大利亚
+        value: 'Australia',
+        label: 'Australia',
+        countryCode: 'au'
+    },
+    {
+        shop: 'global', //马来西亚
+        value: 'Malaysia',
+        label: 'Malaysia',
+        countryCode: 'my'
+    }
+];
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -6,16 +6,34 @@ const instance = axios.create({
   timeout: 5000 // 请求超时时间
 });
 
-// request拦截器
-instance.interceptors.request.use(function (response) {
-    // 2xx范围内的状态码都会触发这个函数
-    // 对响应数据做点什么
-    return response.data;
-}, function (error) {
-    // 非2xx范围内的状态码都会触发这个函数
-    // 对响应错误做点什么
+// 添加请求拦截器
+instance.interceptors.request.use((config) => {
+  // console.log("config:", config);
+  return config;
+},
+  (error) => {
+    // 对请求错误做些什么
     return Promise.reject(error);
-}
+  }
+);
+
+// 添加响应拦截器
+instance.interceptors.response.use(response => {
+  // console.log("response:", response);
+  const { code, status, data, meta } = response.data;
+  if (code === 200) return data;
+  if (status === 200) return meta;
+  else {
+    return Promise.reject(response.data);
+  }
+},
+  (error) => {
+    // 对响应错误做点什么
+    console.log("error-response:", error.response);
+    console.log("error-config:", error.config);
+    console.log("error-request:", error.request);
+    return Promise.reject(error);
+  }
 );
 
 // 暴露这个实例
