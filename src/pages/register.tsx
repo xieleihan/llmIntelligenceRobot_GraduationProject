e079b35e8b30a27ebe381de7964767af7b33f6f6
南秋SouthAki <xieleihan@gmail.com>
2024-12-18 08:32:27 +0800

    feat:新增样式结构化文件和对注册页进行构建

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,8 +1,27 @@
+import '../style/register.scss';
+
+// 导入图标信息
+import ReturnBtn from '../assets/icon/left-heightlight.svg';
+
 function register() {
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <>
+            <div className="register">
+                <div className="returnBtn">
+                    <img src={ ReturnBtn } alt="" />
+                </div>
+
+                <div className='container'>
+                    <div className='title'>注册</div>
+                    <p className='info'>Register</p>
+                    <p className='desc'>探索未知之境</p>
+                    <input className='inputVal' type="text" placeholder="请输入用户名" />
+                    <input className='inputVal' type="email" placeholder="请输入邮箱" />
+                    <input className='inputVal' type="password" placeholder="请输入密码" />
+                    <button className='registerBtn'>注册</button>
+                </div>
+            </div>
+        </>
     );
 }
 
