8cb30cefe75396e19c5ea52e0681d922778a0510
xieleihan <xieleihan@zerozero.cn>
2024-12-19 18:10:25 +0800

    feat:完善后端

--- Back-End/app.js
+++ Back-End/app.js
@@ -2,10 +2,12 @@
 const Koa = require('koa');
 // 导入Koa路由模块
 const Router = require('@koa/router');
+// 导入跨域cors
+const cors = require('@koa/cors');
 
 // 插件
+// 获取环境变量插件
 const dotenv = require('dotenv');
-
 
 // 创建一个Koa对象表示web app本身
 const app = new Koa();
@@ -20,10 +22,15 @@ dotenv.config();
 //   ctx.body = 'Hello World';
 // });
 
-// 检查路由的正常
-router.get('/', async (ctx) => {
-    ctx.body = 'Hello World!';
-});
+// 检查路由的正常(GET)
+// router.get('/test/get', async (ctx) => {
+//     ctx.body = 'Hello World!';
+// });
+
+// 检查路由的正常(POST)
+// router.post('/test/post', async (ctx) => {
+//     ctx.body = 'Hello World!';
+// });
 
 // 使用路由
 app.use(router.routes());
@@ -31,6 +38,9 @@ app.use(router.allowedMethods());
 
 // 静态资源分发
 app.use(require('koa-static')(__dirname + '/public'));
+
+// 使用跨域
+app.use(cors());
 
 // 监听端口
 app.listen(process.env.SERVER_PORT, () => {
