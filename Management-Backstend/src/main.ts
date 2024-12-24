f485b9742f045606fb9a04c72fa1ff21350d9d61
南秋SouthAki <xieleihan@gmail.com>
2024-12-25 00:15:53 +0800

    feat:完善起始页

--- Management-Backstend/src/main.ts
+++ Management-Backstend/src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
 import 'amfe-flexible'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(ElementPlus)
+
+app.mount('#app')
---
+++ Management-Backstend/src/router/index.ts
@@ -0,0 +1,10 @@
+import { createWebHashHistory, createRouter } from 'vue-router';
+
+const routes:any = []
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+export default router;
