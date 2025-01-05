398ec1f10d5b8999d11fdf2aafdf642a646244de
南秋SouthAki <xieleihan@gmail.com>
2025-01-05 21:29:13 +0800

    fix:修复不跟随数组变化自动划到底部的问题

--- Front-End/src/components/Modules/HomeYesinput.tsx
+++ Front-End/src/components/Modules/HomeYesinput.tsx
@@ -2,7 +2,7 @@
 import '../../style/Modules/HomeYesinput.scss';
 
 // 导入React
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 // 导入工具函数
 import { parseTime } from '../../utils/common';
@@ -14,6 +14,7 @@ import SystemIcon from '../../assets/icon/peacock_flat.png';
 function HomeYesinput(dataArray: any) {
     // 定义React变量
     const [nowTime, setNowTime] = useState(''); // 当前时间
+    const messageBoxRef = useRef<HTMLDivElement>(null); // 滚动容器的引用
 
     // 当组件渲染的时候的时间
     useEffect(() => {
@@ -25,10 +26,16 @@ function HomeYesinput(dataArray: any) {
     // console.log("传递的",dataArray)
 
     // 当home-yesinput出现滚动条的时候始终保持在最底部
+    useEffect(() => {
+        if (messageBoxRef.current) {
+            // console.log("触发了", messageBoxRef.current, messageBoxRef.current.scrollHeight);
+            messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
+        }
+    }, [dataArray]);
 
     return (
         <>
-            <div className="home-yesinput">
+            <div className="home-yesinput" ref={messageBoxRef}>
                 <div className="messageBox">
                     <div className="time">
                         { nowTime }
