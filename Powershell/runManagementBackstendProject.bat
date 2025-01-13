a5e420e6ccd29537d896ce3dc58ad055ad24c0bf
xieleihan <xieleihan@gmail.com>
2025-01-13 11:09:27 +0800

    fix:修复bat批处理的问题

---
+++ Powershell/runManagementBackstendProject.bat
@@ -0,0 +1,4 @@
+cd ../Management-Backstend
+call npm i
+set startDir=%cd%
+call npm run dev
--- runStart.bat
+++ runStart.bat
@@ -12,37 +12,46 @@ echo.
 echo ==============功能区======================
 echo 1. 运行 前端React Npm run dev
 echo 2. 运行 后端Koa Npm run dev
-echo 3. 执行 前后端Node modules安装
-echo 4. 运行 VSCode打开Front-End
-echo 5. 退出
+echo 3. 运行 管理后台前端Vue Npm run dev
+echo 4. 执行 前后端Node modules安装
+echo 5. 运行 VSCode打开Front-End
+echo 6. 退出
 echo ========================================
 echo.
 
-set /p choice=请输入选项 (1-5): 
+set /p choice=请输入选项 (1-6): 
 
 if "%choice%"=="1" goto script1
 if "%choice%"=="2" goto script2
 if "%choice%"=="3" goto script3
 if "%choice%"=="4" goto script4
-if "%choice%"=="5" goto exit
+if "%choice%"=="5" goto script5
+if "%choice%"=="6" goto exit
 echo 无效选项，请重试...
 pause
 goto menu
 
 :script1
 start cmd /k "cd ./Powershell && runFrontEndProject.bat"
+pause
 goto menu
 
 :script2
 start cmd /k "cd ./Powershell && runBackEndProject.bat"
+pause
 goto menu
 
 :script3
-start cmd /k "cd ./Powershell && npmInstall.bat"
+start cmd /k "cd ./Powershell && runManagementBackstendProject.bat"
 pause
 goto menu
 
 :script4
+start cmd /k "cd ./Powershell && npmInstall.bat"
+pause
+goto menu
+
+:script5
 start cmd /k "cd ./Front-End && code ."
 pause
 goto menu
