0162459f4aa164144855ef1b6e2a61ad90ac17db
南秋SouthAki <xieleihan@gmail.com>
2025-01-30 23:02:56 +0800

    doc:新年快乐

--- Back-End/router/Modules/deepseekRouter.js
+++ Back-End/router/Modules/deepseekRouter.js
@@ -60,7 +60,7 @@ const moonshotFunc = async function ({question}) {
         messages: [
             {
                 role: "system",
-                content:"你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。"
+                content:"你是 LLM机器人，由 南秋SouthAki 开发的人工智能助手，你更擅长关于GitHub上的各种开源项目的最新动态信息回答。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。南秋SouthAki 为专有名词，不可翻译成其他语言。"
             },
             {
                 role: "user",
